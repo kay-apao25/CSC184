a98a08271befeb16ea847790ac99dd7a4af7c0b2
kay-apao25 <kayapao@yahoo.com>
2014-12-01 20:44:19 +0800

    second successful acceptance test

--- Singleton/test/bdd/features/crawler.feature
+++ Singleton/test/bdd/features/crawler.feature
@@ -1,11 +1,19 @@
 Feature: Singleton Design Pattern Uploads images
          
-         As a user I wish to be able to download pictures
+         As a user I wish to be able to download pictures from a particular website (Example: http://tipidpc.com/)
         
+	Scenario Outline: Cannot Retrieve/Download  images
+	Given I do not run crawler file
+	When it does not get the images from the site
+	Then I cannot see the image "tpcmast.jpg"
+
 	Scenario Outline: Retrieve/Download  images
 	Given I run crawler file
 	When it finishes getting the images from the site
 	Then I can see the image "tpcmast.jpg"
+
+	
+	
 	
      
 	
--- Singleton/test/bdd/steps/steps.py
+++ Singleton/test/bdd/steps/steps.py
@@ -3,7 +3,6 @@ from nose.tools import assert_equal, assert_in
 from crawler import *
 import os
 
-
 @step(u'Given I run crawler file')
 def given_i_run_crawler_file(step):
 	print os.system('python crawler.py')
@@ -16,5 +15,21 @@ def when_it_finishes_getting_the_images_from_the_site(step):
 def then_i_can_see_the_image_group1(step, group1):
     file = os.path.exists("/home/kay/Documents/Singleton/CSC184/Singleton/images/" + group1) 
     assert_equal(file, True)
+
+@step(u'Given I do not run crawler file')
+def given_i_do_not_run_crawler_file(step):
+	assert True
+
+@step(u'Then I cannot see the image "([^"]*)"')
+def then_i_cannot_see_the_image_group1(step, group1):
+	file = os.path.exists("/home/kay/Documents/Singleton/CSC184/Singleton/images/" + group1) 
+	assert_equal(file, False)
+
+@step(u'When it does not get the images from the site')
+def when_it_does_not_get_the_images_from_the_site(step):
+	assert True
+
+
+
 
 
