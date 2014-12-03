10c6dc71fe84e510291adcfeef2dd77ae3141964
kay-apao25 <kayapao@yahoo.com>
2014-12-03 16:52:48 +0800

    added file for facade implementation

--- Singleton/test/bdd/features/crawler.feature
+++ Singleton/test/bdd/features/crawler.feature
@@ -5,12 +5,21 @@ Feature: Singleton Design Pattern Uploads images
 	Scenario Outline: Cannot Retrieve/Download  images
 	Given I do not run crawler file
 	When it does not get the images from the site
-	Then I cannot see the image "tpcmast.jpg"
+	Then I cannot see the image "b.jpg"
 
 	Scenario Outline: Retrieve/Download  images
 	Given I run crawler file
 	When it finishes getting the images from the site
-	Then I can see the image "tpcmast.jpg"
+	Then I can see the following image:
+	|image 	    	  	|
+	|b.jpg			 	|
+	|c.jpg			 	|
+	|ubuntu-logo.png	|
+	|d.jpg				|
+	|k.jpg				|
+	|valid-xhtml10		|
+	|e.jpg				|
+	|f.jpg				|
 
 	
 	
--- Singleton/test/bdd/steps/steps.py
+++ Singleton/test/bdd/steps/steps.py
@@ -3,6 +3,19 @@ from nose.tools import assert_equal, assert_in
 from crawler import *
 import os
 
+@step(u'Given I do not run crawler file')
+def given_i_do_not_run_crawler_file(step):
+	assert True
+
+@step(u'When it does not get the images from the site')
+def when_it_does_not_get_the_images_from_the_site(step):
+	assert True
+
+@step(u'Then I cannot see the image "([^"]*)"')
+def then_i_cannot_see_the_image_group1(step, group1):
+	file = os.path.exists("/home/kay/Documents/Singleton/CSC184/Singleton/images/" + group1) 
+	assert_equal(file, False)
+
 @step(u'Given I run crawler file')
 def given_i_run_crawler_file(step):
 	print os.system('python crawler.py')
@@ -11,23 +24,13 @@ def given_i_run_crawler_file(step):
 def when_it_finishes_getting_the_images_from_the_site(step):
 	assert True
 
-@step(u'Then I can see the image "([^"]*)"')
-def then_i_can_see_the_image_group1(step, group1):
-    file = os.path.exists("/home/kay/Documents/Singleton/CSC184/Singleton/images/" + group1) 
+@step(u'Then I can see the following image:')
+def then_i_can_see_the_following_image(step):
+    file = os.path.exists("/home/kay/Documents/Singleton/CSC184/Singleton/images/") 
     assert_equal(file, True)
 
-@step(u'Given I do not run crawler file')
-def given_i_do_not_run_crawler_file(step):
-	assert True
 
-@step(u'Then I cannot see the image "([^"]*)"')
-def then_i_cannot_see_the_image_group1(step, group1):
-	file = os.path.exists("/home/kay/Documents/Singleton/CSC184/Singleton/images/" + group1) 
-	assert_equal(file, False)
 
-@step(u'When it does not get the images from the site')
-def when_it_does_not_get_the_images_from_the_site(step):
-	assert True
 
 
 
